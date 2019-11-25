9714cef87ca26a9099210a1e078bf35fadddcf1b
Jakub Kolodziejczyk <jakub.kolodziejczyk@mail.com>
2019-11-25 23:14:06 +0900

    Replace bash scripts with invoke
    A far superior solution

--- tests/commit_stage/run_commit_stage.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-set -e
-
-bash ./tests/commit_stage/run_unit_tests.sh
-bash ./tests/commit_stage/run_static_tests_analysis.sh
--- tests/commit_stage/run_unit_tests.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-set -e
-
-source $HOME/anaconda3/etc/profile.d/conda.sh
-conda activate voc_ssd_environment
-
-py.test -s
